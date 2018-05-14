659338ec5837e30bfb1fbd4e866c425f2face6ba
xrust <xrust@github.com>
2018-05-15 01:12:02 +0200

    Fix image directory structure

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from views import index
 
 
 def setup_static_routes(app):
-    app.router.add_static('/images/', path='app/images', name='img')
+    app.router.add_static('/img/', path='app/img', name='img')
     app.router.add_static('/css/', path='app/css', name='css')
     app.router.add_static('/js/', path='app/js', name='js')
 
