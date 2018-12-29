97375dbb12600b1ce668aa3c8f395d5a50c2dd67
xrust <xrust@github.com>
2018-12-30 00:49:00 +0100

    Add TLS support

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ import aiohttp_jinja2
 import jinja2
 import argparse
 import yaml
+import ssl
 
 
 def parse_args():
@@ -18,22 +19,36 @@ def load_config(config):
     with open(config, 'r') as stream:
         return yaml.safe_load(stream)
 
+def tls(config):
+    if config['tls']['enable']:
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        context.load_cert_chain(
+            config['tls']['certificate'],
+            config['tls']['key'],
+        )
+    else:
+        context = None
 
-# config
-args = parse_args()
-config = load_config(args.config)
+    return context
 
-# database
-print('SQLite {}'.format(sql('select sqlite_version()')[0][0]))
+def main():
+    # config
+    args = parse_args()
+    config = load_config(args.config)
 
-# bot
-# threading.Thread(target=bot).start()
+    # database
+    # print('SQLite {}'.format(sql('select sqlite_version()')[0][0]))
 
-# webapp
-app = web.Application()
-app.on_response_prepare.append(on_response_prepare)
-setup_static_routes(app)
-setup_routes(app)
-aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('app/templates'))
-web.run_app(app, host=config['host'], port=config['port'])
+    # bot
+    # threading.Thread(target=bot).start()
 
+    # webapp
+    app = web.Application()
+    app.on_response_prepare.append(on_response_prepare)
+    setup_static_routes(app)
+    setup_routes(app)
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('app/templates'))
+    web.run_app(app, host=config['host'], port=config['port'], ssl_context=tls(config))
+
+
+main()
