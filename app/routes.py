f6d7878183d34fdbf51e86a3ce9084367f760bf9
xrust <xrust@github.com>
2019-03-18 20:52:34 +0100

    Add POST example

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from views import index
+from views import index_get, index_post
 
 
 def setup_static_routes(app):
@@ -7,4 +7,5 @@ def setup_static_routes(app):
     app.router.add_static('/js/', path='app/js', name='js')
 
 def setup_routes(app):
-    app.router.add_get('/', index)
+    app.router.add_get('/', index_get)
+    app.router.add_post('/', index_post)
--- app/views.py
+++ app/views.py
@@ -3,7 +3,12 @@ import aiohttp_jinja2
 import jinja2
 import json
 
-async def index(request):
+async def index_get(request):
     return aiohttp_jinja2.render_template('index.html', request, {
         'message': 'Online'
+    })
+
+async def index_post(request):
+    return aiohttp_jinja2.render_template('index.html', request, {
+        'message': json.dumps(await request.json(), indent=4)
     })
